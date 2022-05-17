a8e47f87377b406c1a852725fccc105a9915447a
Adrian Oprea <adrianoprea@Adrians-MacBook-Pro.local>
2022-05-17 17:11:29 +1000

    adds

--- Chapter-7/Local-Install/Setup-Jenkins-Local.sh
+++ Chapter-7/Local-Install/Setup-Jenkins-Local.sh
@@ -1,8 +1,26 @@
 # Install Jenkins 
 brew install jenkins-lts
 
+# In my Case Mac M1
+arch -x86_64 brew install jenkins-lts
+
+# Uninstall
+arch -x86_64 brew uninstall jenkins-lts
+
+
 # Will start jenkins on port 8080
 brew services start jenkins-lts
+sudo arch -x86_64 brew services start jenkins-lts
 
 # Restart Jenkins
 brew services restart jenkins-lts
+sudo arch -x86_64  brew services restart jenkins-lts
+
+# Stop Jenkins
+brew services stop jenkins-lts
+sudo arch -x86_64 brew services stop jenkins-lts
+
+
+# Check services status 
+brew services list 
+sudo arch -x86_64 brew services list 
--- Chapter-7/Local-Install/utils.sh
+++ Chapter-7/Local-Install/utils.sh
@@ -5,15 +5,22 @@ echo " Start NiFi STG"
 /opt/nifi-stg/bin/nifi.sh start
 echo " Start NiFi DEV"
 /opt/nifi-dev/bin/nifi.sh start
+echo " Start NiFi Registry"
+/opt/nifi-registry/bin/nifi-registry.sh start
+echo " Start Jenkins"
+brew services start jenkins-lts
 
 
-echo " Start NiFi PRD"
+echo " Stop NiFi PRD"
 /opt/nifi-prd/bin/nifi.sh stop
-echo " Start NiFi STG"
+echo " Stop NiFi STG"
 /opt/nifi-stg/bin/nifi.sh stop
-echo " Start NiFi DEV"
+echo " Stop NiFi DEV"
 /opt/nifi-dev/bin/nifi.sh stop
-
+echo " Stop NiFi Registry"
+/opt/nifi-registry/bin/nifi-registry.sh stop
+echo " Stop Jenkins"
+brew services stop jenkins-lts
 
 echo " Remove all NiFi Setup"
 # rm -rf /opt/nifi-prd/ /opt/nifi-stg/ /opt/nifi-dev/ /opt/nifi-registry/ /opt/nifi-toolkit/ 
